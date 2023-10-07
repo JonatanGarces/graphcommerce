4a49ee03170b432bc55d7afc1c474dfee03d20f0
Jonatan Garces <jogala14@gmail.com>
2023-10-06 20:09:30 -0600

    feature/T-231004

--- packagesDev/eslint-config/index.js
+++ packagesDev/eslint-config/index.js
@@ -125,13 +125,11 @@ module.exports = {
           {
             name: '@mui/material',
             importNames: [
-              'Autocomplete',
               'Checkbox',
               'Radio',
               'Select',
               'Slider',
               'Switch',
-              'TextField',
               'ToggleButtonGroup',
             ],
             message: `Please use \`import { SelectElement, TextFieldElement, etc } from '@graphcommerce/ecommerce-ui'\` for usage in forms. https://github.com/graphcommerce-org/graphcommerce/tree/main/packages/ecommerce-ui/components/FormComponents`,
